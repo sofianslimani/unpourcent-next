7a48347d36064db8aa874e01fa36a99036d5ea4f
Thomas Cardiet <thom_car@yahoo.fr>
2023-06-06 23:54:53 +0200

    feat: add heroAssociation and SocialSection

---
+++ pages/association.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { HeroAssociation } from '../components/HeroAssociation';
+import { SocialSection } from '../components/SocialSection';
+
+function Association() {
+  return (
+    <div>
+      <HeroAssociation />
+      <SocialSection />
+    </div>
+  );
+}
+
+export default Association;
