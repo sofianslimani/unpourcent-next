746d67f98d59785ce60a98dc74b1905243be9a01
Sofian Slimani <70762272+Sev7n2507@users.noreply.github.com>
2023-06-07 20:49:29 +0200

    fix: fix ContactHero style

--- pages/contact/Partials/ContactHero.js
+++ pages/contact/Partials/ContactHero.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import { MaxWidth } from '../../../components/MaxWidth';
 
 const ContactHero = () => {
   return (
     <section className={'contact-hero'}>
-      <MaxWidth>
-        <div className={'contact-hero-container'}>
-          <div className={'contact-hero-container-left'}>
-            <h1 className={'contact-hero-container-left-title black'}>
-              Contact
-            </h1>
-            <p>Contact Remplissez le formulaire pour nous contacter</p>
-          </div>
-          <div className={'contact-hero-container-right'}>
-            <img
-              src="./illustrations/hero-contact-1pourcent-2.png"
-              alt="hero"
-            />
-          </div>
+      <div className={'contact-hero-container'}>
+        <div className={'contact-hero-container-left'}>
+          <h1 className={'contact-hero-container-left-title black'}>Contact</h1>
+          <p>Contact Remplissez le formulaire pour nous contacter</p>
         </div>
-      </MaxWidth>
+        <div className={'contact-hero-container-right'}>
+          <img src="./illustrations/hero-contact-1pourcent-2.png" alt="hero" />
+        </div>
+      </div>
     </section>
   );
 };
