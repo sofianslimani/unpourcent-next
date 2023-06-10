739c9243781d244a8adaf4f7bc1c56fd491401f0
Sofian Slimani <70762272+Sev7n2507@users.noreply.github.com>
2023-06-10 21:34:08 +0200

    fix: add form on ContactForm section

--- pages/contact/Partials/ContactForm.js
+++ pages/contact/Partials/ContactForm.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { MaxWidth } from '../../../components/MaxWidth';
+import { useFormik } from 'formik';
 
 const ContactForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      message: '',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <section className={'contact-form'}>
       <MaxWidth>
@@ -14,14 +27,99 @@ const ContactForm = () => {
           </div>
           <div className={'contact-form-container-right'}>
             <div className="contact-form-container-right-top">
-              <h2>Nous contacter</h2>
+              <h2 className={'bold'}>Nous contacter</h2>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam
                 duis vitae curabitur amet, fermentum lorem.
               </p>
             </div>
             <div className="contact-form-container-right-bottom">
-              <form></form>
+              <form onSubmit={formik.handleSubmit}>
+                <div className="contact-form-container-right-bottom-inputs">
+                  <div className="contact-form-container-right-bottom-inputs-input">
+                    <label className={'bold'} htmlFor="firstName">
+                      First Name <span className="red">*</span>
+                    </label>
+
+                    <input
+                      id="firstName"
+                      name="firstName"
+                      placeholder="First Name"
+                      type="text"
+                      onChange={formik.handleChange}
+                      value={formik.values.firstName}
+                      required
+                    />
+                  </div>
+                  <div className="contact-form-container-right-bottom-inputs-input">
+                    <label className={'bold'} htmlFor="lastName">
+                      Last Name <span className="red">*</span>
+                    </label>
+
+                    <input
+                      id="lastName"
+                      name="lastName"
+                      placeholder="Last Name"
+                      type="text"
+                      onChange={formik.handleChange}
+                      value={formik.values.lastName}
+                      required
+                    />
+                  </div>
+                </div>
+
+                <div className="contact-form-container-right-bottom-input">
+                  <label className={'bold'} htmlFor="email">
+                    E-mail <span className="red">*</span>
+                  </label>
+
+                  <input
+                    id="email"
+                    name="email"
+                    placeholder="yourmail@gmail.com"
+                    type="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                    required
+                  />
+                </div>
+
+                <div className="contact-form-container-right-bottom-input">
+                  <label className={'bold'} htmlFor="phone">
+                    Phone <span className="red">*</span>
+                  </label>
+
+                  <input
+                    id="phone"
+                    name="phone"
+                    placeholder="06 12 34 56 78"
+                    type="phone"
+                    onChange={formik.handleChange}
+                    value={formik.values.phone}
+                    required
+                  />
+                </div>
+
+                <div className="contact-form-container-right-bottom-input">
+                  <label className={'bold'} htmlFor="message">
+                    Message <span className="red">*</span>
+                  </label>
+
+                  <textarea
+                    id="message"
+                    name="message"
+                    placeholder="Your message"
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.message}
+                    required
+                  />
+                </div>
+
+                <button type="submit" className={'button-primary'}>
+                  Envoyer
+                </button>
+              </form>
             </div>
           </div>
         </div>
