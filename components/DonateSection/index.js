f6a2860b6965172ede6ee8817874723908ef5d8c
Thomas Cardiet <thom_car@yahoo.fr>
2023-06-23 11:40:30 +0200

    merge main

--- components/DonateSection/index.js
+++ components/DonateSection/index.js
@@ -1,27 +1,26 @@
 import Link from 'next/link';
 import React from 'react';
-import Link from 'next/link';
 import { MaxWidth } from '../MaxWidth';
 
 export const DonateSection = ({
-  title, description, isExternal, link, linkText
+  title,
+  description,
+  isExternal,
+  link,
+  linkText,
 }) => {
   return (
     <section className="donate-section">
       <MaxWidth>
-        <h2>{ title }</h2>
-        <p>{ description }</p>
-        {
-          isExternal ? (
-            <a target='_blank' href={link}>
-              { linkText }
-            </a>
-          ) : (
-            <Link href={link}>
-              { linkText }
-            </Link>
-          )
-        }
+        <h2>{title}</h2>
+        <p>{description}</p>
+        {isExternal ? (
+          <a target="_blank" href={link}>
+            {linkText}
+          </a>
+        ) : (
+          <Link href={link}>{linkText}</Link>
+        )}
       </MaxWidth>
     </section>
   );
