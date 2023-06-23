7a98b524db3565c684da7b4b5d36819da7a9506f
SamyMahmoudi <mahmoudi-samy-pro@outlook.fr>
2023-06-23 12:03:08 +0200

    update cta contact

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,24 +1,28 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import ContactHero from './Partials/ContactHero';
 import ContactForm from './Partials/ContactForm';
 import ContactFaq from './Partials/ContactFaq';
-import ContactBanner from './Partials/ContactBanner';
 import { DonateSection } from '../../components/DonateSection';
 
 const ContactPage = () => {
-    
-    
-    useEffect(() => {
-        document.title = "Un Pour Cent - Contact";
-    }, []);
-    
-    
-    return (
+
+  useEffect(() => {
+    document.title = "Un Pour Cent - Contact";
+  }, []);
+
+
+  return (
     <>
       <ContactHero />
       <ContactForm />
       <ContactFaq />
-      <ContactBanner />
+      <DonateSection
+        title={'Découvrez dès maintenant le projet du moment'}
+        description={'Découvrez dès maintenant le projet associatif mis en avant ce mois-ci sur notre plateforme. Soyez inspiré par son impact et participez à cette belle aventure de solidarité.'}
+        link={'/projet-du-mois'}
+        linkText={'Découvrir le projet'}
+        isExternal={false}
+      />
     </>
   );
 };
