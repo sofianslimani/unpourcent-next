1b2275a39af630af9adbae3854d418186b43acf8
Thomas Cardiet <thom_car@yahoo.fr>
2023-06-23 10:38:24 +0200

    fix grey background section

--- layouts/Sections/SectionImagesAndText.js
+++ layouts/Sections/SectionImagesAndText.js
@@ -1,39 +1,55 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
-const SectionImgaesAndText = ({data, greyBackground, customClasses, inverse}) => {
-	
-	
-	const [content, setContent] = useState({});
-	
-	useEffect(() => {
-		if (data){
-			setContent(data)
-		}
-	}, []);
-	
-	
-	return (
-		<section className={"section-images-and-text container-l " + (greyBackground ? "grey-background " :"") + (customClasses ? customClasses : " ")}>
-			<aside className={"section-images-and-text-container" + (inverse ? " inverse" : " ")}>
-				<div className={`section-images-and-text-container-img ` + (content.isGrid ? "grid " :" ")}>
-					{content.isGrid ?
-						content.images.map((itemImg,index)=>{
-							return (
-								<img className={"item-img"} key={index} src={itemImg}/>
-							)
-						})
-						: content.images && content.images[0] &&
-						<img className={"item-img"} src={content.images[0]}/>
-					}
-				</div>
-				<div className={"section-images-and-text-container-text"}>
-					{content.title}
-					<p className={"text-16 color-black regular"}>{content.text}</p>
-					{content.cta}
-				</div>
-			</aside>
-		</section>
-	);
+const SectionImgaesAndText = ({
+  data,
+  greyBackground,
+  customClasses,
+  inverse,
+}) => {
+  const [content, setContent] = useState({});
+
+  useEffect(() => {
+    if (data) {
+      setContent(data);
+    }
+  }, []);
+
+  return (
+    <section
+      className={
+        'section-images-and-text container-l ' +
+        (greyBackground ? 'grey-background ' : '') +
+        (customClasses ? customClasses : ' ')
+      }
+    >
+      <aside
+        className={
+          'section-images-and-text-container' + (inverse ? ' inverse' : ' ')
+        }
+      >
+        <div
+          className={
+            `section-images-and-text-container-img ` +
+            (content.isGrid ? 'grid ' : ' ')
+          }
+        >
+          {content.isGrid
+            ? content.images.map((itemImg, index) => {
+                return <img className={'item-img'} key={index} src={itemImg} />;
+              })
+            : content.images &&
+              content.images[0] && (
+                <img className={'item-img'} src={content.images[0]} />
+              )}
+        </div>
+        <div className={'section-images-and-text-container-text'}>
+          {content.title}
+          <p className={'text-16 color-black regular'}>{content.text}</p>
+          {content.cta}
+        </div>
+      </aside>
+    </section>
+  );
 };
 
 export default SectionImgaesAndText;
