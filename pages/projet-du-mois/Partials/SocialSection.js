9108a653b3ae2071920be4d46b5af5eda607e5c8
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2023-07-26 17:19:16 +0200

    [axel/eslint-config][config]: config eslint

--- pages/projet-du-mois/Partials/SocialSection.js
+++ pages/projet-du-mois/Partials/SocialSection.js
@@ -48,7 +48,7 @@ function SocialSection() {
           className="socials__card socials__card--linkedin"
           href="https://www.linkedin.com/company/amatullah-au-service-de-l-humanité/"
         >
-          <Image
+          <img
             width={40}
             height={40}
             alt="linkedin"
